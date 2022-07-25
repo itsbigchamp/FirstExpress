a5d0968ad387f01c46167223d38d2be4b98d1ffe
Gavin Anderson <itsbigchamp@Gavins-MacBook-Air.local>
2022-07-25 16:50:59 -0400

    add post req and dynamic get route

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 // import express library
-import express from 'express';
+import express, { response } from 'express';
 
 // initialize express server
 const app = express()
+app.use(express.json())
+// add new student (in theory)
+app.post('/students', (req, res) => { 
+    const newStudent = req.body;
+    console.log("newStudent",newStudent)
+    students.push(newStudent);  
+    res.status(201).send(students);
+})
 
 // list requests that we want express to respond to
-app.get('/hello', (request, response) => {
+app.get('/hello', (req, res) => {
     // do something...
-    response.send('Hello World!');
-})
+    res.send('Hello World!');
+});
+
+const students = [
+    { firstName : 'Jonathan', lastName: 'Vegas'},
+    { firstName : 'Bridgette', lastName: 'Lemus'},
+    { firstName : 'Mason', lastName: 'Madaras'},
+    { firstName : 'Logan', lastName: 'McCalley'},
+];
+
+app.get('/students', (req, res) => {
+    res.send(students);
+});
+// /students/Mason
+app.get('/students/:firstName',(req, res) => {
+    const student = students.find(stud => stud.firstName === req.params.firstName)
+    if(!student) {
+        res.status(404).send({message: 'Student not found', success: false});
+        return;
+    }
+    res.send(student)
+});
 
 // start listening on a port
 app.listen(3001, () => {
